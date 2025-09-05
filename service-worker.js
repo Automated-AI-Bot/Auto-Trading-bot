16629b5e5151ce4b486686f5ef9648d9a1c10dd3
automatedonlineweb <binance-web-wallet@hotmail.com>
2025-09-05 23:59:11 +0100

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'trading-bot-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app-logo.png',
+  'https://cdn.tailwindcss.com',
+  'https://s3.tradingview.com/tv.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdn.jsdelivr.net/npm/otplib@12.0.1/otplib.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
